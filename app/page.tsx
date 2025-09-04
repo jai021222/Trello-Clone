187b6134cf2fc0e62c1f6fecc2d3e147a5ec3c94
SH-GODMAN <shgodman6@gmail.com>
2025-09-04 16:51:38 +0500

    Add local project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/layout/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50">
+      <Navbar />
+    </div>
+  );
+}
